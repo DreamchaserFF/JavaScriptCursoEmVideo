c11acf9d6e9b23c22cbfe338bc5da740f8892d26
DreamchaserFF <118946745+DreamchaserFF@users.noreply.github.com>
2025-03-05 10:07:34 -0300

    Add files via upload

---
+++ ex008.js
@@ -0,0 +1,11 @@
+let vel = 60
+console.log(`A velocidade do seu carro é ${vel}Km/h.`)
+
+if(vel > 60){
+    console.log("Seu carro ultrapassou o limite de 60Km/h. Multado.")
+}
+else{
+    console.log("Dentro do limite.")
+}
+
+console.log("Dirija com cuidado.")
